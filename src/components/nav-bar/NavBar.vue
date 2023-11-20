05f61a6159ad0ff82b7f3bff616e0bf30ad03d57
vishseen <vishseenarain@gmail.com>
2023-11-20 18:50:16 +0400

    [DEV] - Initialised vue3 repo.

---
+++ src/components/nav-bar/NavBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+//  import { NavItem } from '../../types/types';
+
+    defineProps<{
+        logoTxt: string;
+        // navItems: NavItem[];
+    }>()
+</script>
+
+
+<template>
+    <div class="nav-bar__wrapper">
+        <div class="logo">
+            {{ logoTxt }}
+        </div>
+
+        <nav class="nav-bar">
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+</template>
